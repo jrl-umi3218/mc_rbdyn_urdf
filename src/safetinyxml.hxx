cd8eaed7a3d47fedd14d74f0ad4cf9927c47c3a7
Hervé Audren <h.audren@gmail.com>
2017-06-15 18:15:17 +0900

    Add some more queries

--- src/safetinyxml.hxx
+++ src/safetinyxml.hxx
@@ -7,6 +7,7 @@ namespace mc_rbdyn_urdf
 {
   using MaybeElement = either<std::string, tinyxml2::XMLElement*>;
   using MaybeDouble = either<std::string, double>;
+  using MaybeString = either<std::string, std::string>;
 
   std::function<MaybeElement(tinyxml2::XMLElement*)>
   firstChildElement(const std::string& tag)
@@ -31,6 +32,34 @@ namespace mc_rbdyn_urdf
     return [attr](tinyxml2::XMLElement* elem)
     {
       return MaybeDouble::right(elem->DoubleAttribute(attr.c_str()));
+    };
+  }
+
+  std::function<MaybeDouble(tinyxml2::XMLElement*)>
+  doubleAttribute(const std::string& attr, double def)
+  {
+    return [attr, def](tinyxml2::XMLElement* elem)
+    {
+      double res = def;
+      elem->QueryDoubleAttribute(attr.c_str(), &res);
+      return MaybeDouble::right(res);
+    };
+  }
+
+  std::function<MaybeString(tinyxml2::XMLElement*)>
+  attribute(const std::string& attr)
+  {
+    return [attr](tinyxml2::XMLElement* elem)
+    {
+      auto ret = elem->Attribute(attr.c_str());
+      if(ret)
+      {
+        return MaybeString::right(ret);
+      }
+      else
+      {
+        return MaybeString::left("No attribute named " + attr);
+      }
     };
   }
 }
