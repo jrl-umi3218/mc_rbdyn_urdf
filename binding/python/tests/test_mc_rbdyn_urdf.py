1bb8cd14bdfa7d9e44ca55b5ecf264aa945af915
Pierre Gergondet <pierre.gergondet@gmail.com>
2019-04-05 16:25:03 +0900

    Change license to BSD-2-Clause

--- binding/python/tests/test_mc_rbdyn_urdf.py
+++ binding/python/tests/test_mc_rbdyn_urdf.py
@@ -1,21 +1,8 @@
 #!/usr/bin/env python
 
-# Copyright 2015-2017 CNRS-UM LIRMM, CNRS-AIST JRL
 #
-# This file is part of mc_rbdyn_urdf.
+# Copyright 2012-2019 CNRS-UM LIRMM, CNRS-AIST JRL
 #
-# mc_rbdyn_urdf is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# mc_rbdyn_urdf is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with mc_rbdyn_urdf.  If not, see <http://www.gnu.org/licenses/>.
 
 import unittest
 
